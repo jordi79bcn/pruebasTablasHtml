70b26d7619d38d0f7b12efd4d72ba91d7fee28a6
Jordi Escobar <jordi79.trabajo@gmail.com>
2022-12-21 11:40:24 +0100

    añadidas 2 clases celda verde y celda roja. Ahora puedo añadir una class al hacer click y eso cambia el fondo de la celda original.

--- PruebasMapa/src/main/webapp/js/funciones.js
+++ PruebasMapa/src/main/webapp/js/funciones.js
@@ -4,13 +4,9 @@
 const CLICK_IZQUIERDO = 1;
 
  function clickCelda(numCelda){
-	 //alert("click en: " + numCelda);
-	 //document.getElementById(numCelda)
-	 //var img = document.getElementById(numCelda).getElementsByClassName("imgMapa")[0];
-	 //img.src="img/rojo.png";	 
-	 var div= document.getElementById(numCelda);
-	 div.style.back
-	 img.src="img/rojo.png";	 
+	//alert("click en: " + numCelda);
+	var div= document.getElementById(numCelda);
+	div.classList.add("roja");
  }
  
  function entrarCelda(event){
