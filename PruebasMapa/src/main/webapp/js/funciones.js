8b8803f48a993b3ec5c4ac7bfc1a24a620b5f7c7
Jordi Escobar <jordi79.trabajo@gmail.com>
2022-12-21 11:50:55 +0100

    añadido modo insertar en javascript. Permite 1 click y colorear hasta volver a hacer click.

--- PruebasMapa/src/main/webapp/js/funciones.js
+++ PruebasMapa/src/main/webapp/js/funciones.js
@@ -2,19 +2,32 @@
  * 
  */
 const CLICK_IZQUIERDO = 1;
+const MODO_NADA = 0;
+const MODO_INSERTAR = 1;
 
- function clickCelda(numCelda){
-	//alert("click en: " + numCelda);
-	var div= document.getElementById(numCelda);
-	div.classList.add("roja");
+var modo = MODO_NADA;
+
+ function clickCelda(idCelda){
+	if (modo == MODO_NADA)
+		modo = MODO_INSERTAR;
+	else if (modo == MODO_INSERTAR)
+		modo = MODO_NADA;
+	else
+		alert("error! modo imposible: " + modo);
+		
+	marcarCelda(idCelda);
  }
  
  function entrarCelda(event){
-	 //alert("click en: " + numCelda);
-	 //document.getElementById(numCelda)
-	 if (event.buttons == CLICK_IZQUIERDO){
-	 	console.log(event.target.id);
+	 console.log(event.target.id);
+	 if (modo == MODO_INSERTAR){
+	 	marcarCelda(event.target.id);
 	 }
+ }
+ 
+ function marcarCelda(idCelda){
+	var div= document.getElementById(idCelda);
+	div.classList.add("roja");
  }
  
  
